278e842998459364931eadda0a7c9ec2ab7b7a90
PabloMB05 <pmerino@itpfp.com>
2025-05-14 14:49:31 +0200

    Registro arreglado

--- src/App/Auth/Controllers/RegisteredUserController.php
+++ src/App/Auth/Controllers/RegisteredUserController.php
@@ -3,7 +3,7 @@
 namespace App\Auth\Controllers;
 
 use App\Core\Controllers\Controller;
-use App\Models\User;
+use Domain\Users\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
